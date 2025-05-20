bea9d6b50d7cd1179536dfeb3168c778cefc77c9
sivakrishnavemula <siva8019209422@gmail.com>
2025-05-20 09:47:18 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -16,3 +16,13 @@ aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local 
 export KOPS_STATE_STORE=s3://sivakrishna.flm.k8s.local
 kops create cluster --name siva.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --master-volume-size 30 --node-size t2.micro --node-count 2 --node-volume-size 25
 kops update cluster --name siva.k8s.local --yes --admin
+
+
+#Suggestions:
+ #* validate cluster: kops validate cluster --wait 10m
+ #* list nodes: kubectl get nodes --show-labels
+ #* ssh to a control-plane node: ssh -i ~/.ssh/id_rsa ubuntu@
+ #* the ubuntu user is specific to Ubuntu. If not using Ubuntu please use the appropriate user based on your OS.
+ #* read about installing addons at: https://kops.sigs.k8s.io/addons.
+
+
